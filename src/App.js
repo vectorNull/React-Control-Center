779b3be4c1a7e283883d56a3c0a04ac02b9d4982
Jose Rodriguez <jose.rodriguez@prominentcode.com>
2022-06-03 15:05:13 -0700

    App setup

--- src/App.js
+++ src/App.js
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useEffect } from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { FiSettings } from 'react-icons/fi';
+import { ToolTipsComponent } from '@syncfusion/ej2-react-popups';
 
-import './App.css'
+import './App.css';
 
 const App = () => {
-  return (
-    <h1 className='underline text-3xl'>App</h1>
-  )
+    return (
+        <h1 className='underline text-3xl'>App</h1>
+    )
 }
 
 export default App
